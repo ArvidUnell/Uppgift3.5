110f0e6ec3dd9c4b3e252e7223175c7955416b7f
arun06001 <arun06001@E-PW0234KE>
2023-10-06 10:15:12 +0200

    Add project files.

---
+++ Uppgift3.5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Uppgift3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två tal separerade med ett semikolon");
+            string talStr = Console.ReadLine();
+
+            float tal1 = float.Parse(talStr[..talStr.IndexOf(';')]);
+            float tal2 = float.Parse(talStr[(talStr.IndexOf(";")+1)..]);
+
+            Console.WriteLine("Välj ett räknesätt");
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraktion");
+            Console.WriteLine("3: Multiplikation");
+            Console.WriteLine("4: Division");
+            byte räknesätt = byte.Parse(Console.ReadLine());
+
+            switch (räknesätt)
+            {
+                case 1:                  
+                    Console.WriteLine($"Svar: {tal1 + tal2}.");
+                    break;
+
+                case 2:
+                    Console.WriteLine($"Svar: {tal1 - tal2}.");
+                    break;
+
+                case 3:
+                    Console.WriteLine($"Svar: {tal1 * tal2}.");
+                    break;
+
+                case 4:
+                    Console.WriteLine($"Svar: {tal1 / tal2}.");
+                    break;
+
+                default:
+                    Console.WriteLine($"{räknesätt} är inte ett giltigt räknesätt.");
+                    break;
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta.");
+            Console.ReadKey();
+        }
+    }
+}
